23396f14162df6d03168d3b51f92291b26896c26
theFarhanZaman <mdfarhanzamanofficial@gmail.com>
2026-05-31 01:10:17 +0600

    Add battery voltage measurement to SensorManager
    Added battery voltage measurement and updated constructor.

--- SolarTracker/SensorManager.h
+++ SolarTracker/SensorManager.h
@@ -13,6 +13,7 @@ private:
     uint8_t inaAddr;
     float shuntOhms;
     float maxCurrent;
+    uint8_t batPin; // Resistor divider analog pin
 
 public:
     Adafruit_MPU6050 mpu;
@@ -20,10 +21,13 @@ public:
     INA226 ina;
 
     float temperature, humidity, pressure;
-    float busVoltage, currentmA, powerW;
+    float busVoltage, currentmA, powerW;       // Measures Solar Panel output
+    float batteryVoltage;                       // Measures LiPo status
+    bool systemCritical;                        // True if battery is under 3.2V
 
-    SensorManager(uint8_t sda, uint8_t scl, uint8_t bme_a = 0x76, uint8_t ina_a = 0x40, float shunt = 0.1f, float max_i = 1.0f);
-
+    // Added bat_p to the constructor signature
+    SensorManager(uint8_t sda, uint8_t scl, uint8_t bat_p, uint8_t bme_a = 0x76, uint8_t ina_a = 0x40, float shunt = 0.1f, float max_i = 1.0f);
+    
     bool init();
     void update();
 };
